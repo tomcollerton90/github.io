78526375c8f4e2abbe2b699658fafe0d30007ba6
Neeraj Verma <nverma31@gmail.com>
2019-12-03 11:07:03 +0100

    adding interactivity

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,100 @@
-const menuIconEl = $('.menu-icon');
-const sidenavEl = $('.sidenav');
-const sidenavCloseEl = $('.sidenav__close-icon');
+var para = {
+  duration: 2000,
+  delay: 200,
 
-// Add and remove provided class names
-function toggleClassName(el, className) {
-  if (el.hasClass(className)) {
-    el.removeClass(className);
-  } else {
-    el.addClass(className);
-  }
+  origin: 'left',
+  scale: 0,
+  opacity: 0
 }
 
-// Open the side nav on click
-menuIconEl.on('click', function() {
-  toggleClassName(sidenavEl, 'active');
-});
+var paraheader = {
+  duration: 2000,
 
-// Close the side nav on click
-sidenavCloseEl.on('click', function() {
-  toggleClassName(sidenavEl, 'active');
+  origin: 'left',
+  scale: 0,
+  opacity: 0
+}
+
+var imag = {
+      duration: 2500,
+      delay: 100,
+      opacity: 0
+
+     
+
+  }
+  var longimag = {
+      duration: 1500,
+
+      scale: 0,
+      opacity: 0
+
+  }
+  //Scroll Reveal  ------------>
+
+$(document).ready(function() {
+  window.sr = ScrollReveal();
+
+  //   sr.reveal('.design-image img', {
+  //   afterReveal: function(el) {
+  //   anime({
+  //           targets: el,
+  //           easing: 'linear',
+
+  //               width: [
+  //                   {value: ['-800%','100%'], duration: 1000 }
+  //               ]
+  //       });    }
+  // });
+
+
+  // sr.reveal('.thumbnail ', images);
+
+  sr.reveal('h1', imag);
+  // sr.reveal('p', para);
+
+  // sr.reveal('p', para);
+
+
+
+  // sr.reveal('img', imag);
+  // sr.reveal('project-img-long', longimag);
+
+  // sr.reveal('h3', imag);
+  // sr.reveal('h4', imag);
+
+  // var animeRevealerOpts: {
+  //                 easing: 'easeOutCubic',
+  //                 delay: function(t,i) {
+  //                     return i*100;
+  //                 },
+  //                 translateX: [
+  //                     {value: ['101%','0%'], duration: 400 },
+  //                     {value: ['0%','-101%'], duration: 400}
+  //                 ]
+  //             };
+  //             var animeOpts: {
+  //                 duration: 900,
+  //                 easing: 'easeOutCubic',
+  //                 delay: function(t,i) {
+  //                     return 400+i*100;
+  //                 },
+  //                 opacity: {
+  //                     value: 1,
+  //                     duration: 1,
+  //                     easing: 'linear'
+  //                 },
+  //                 scale: [0.8,1]
+  //             };
+
+
+ 
+
+
+  
+
+
+
+
+
 });
